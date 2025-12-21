21fb5a017a509238fc1a8a45cffdd917e209e0b2
joaquinfontana <fontana200514@gmail.com>
2025-12-21 11:59:48 -0300

    side nav

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,17 +1,32 @@
-import { Component, signal,inject } from '@angular/core';
+import { Component, signal,inject, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '@shared/header/header';
-import {MatSidenavModule} from '@angular/material/sidenav'
+import { MatSidenav, MatSidenavModule} from '@angular/material/sidenav'
 import { Sidenav } from '@shared/sidenav/sidenav';
 import { AuthStore } from '@auth-store';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet,Header,MatSidenavModule,Sidenav],
+  imports: [RouterOutlet,Header,MatSidenavModule,Sidenav,MatIconModule,MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('alquileres-maria-frontend');
   readonly authStore = inject(AuthStore);
+  sidenavOpened = signal(false);
+  
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  toggleSidenav(): void {
+    if (this.sidenav) {
+      this.sidenav.toggle();
+    }
+  }
+
+  onSidenavChange(opened: boolean): void {
+    this.sidenavOpened.set(opened);
+  }
 }
